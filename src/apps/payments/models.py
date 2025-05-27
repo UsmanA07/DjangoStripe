bf99121aad129ca19878c09f3125b36c328f25cb
usman <usmanakkaev@gmail.com>
2025-05-27 14:10:51 +0300

    add: model Item

--- src/apps/payments/models.py
+++ src/apps/payments/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name='Название'
+    )
+    description = models.TextField(
+        blank=True,
+        verbose_name='Описание'
+    )
+    price = models.PositiveIntegerField(
+        verbose_name='Цена',
+        help_text='Укажите цену в целых числах, например, в копейках.'
+    )
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return f'{self.name} - {self.price}'
+
+    def get_price_display(self):
+        return f'{self.price // 100}'
