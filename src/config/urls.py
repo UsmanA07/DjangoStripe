054316ce69c9794557ec35960012299b83c2f4cb
usman <usmanakkaev@gmail.com>
2025-05-31 16:25:07 +0300

    fix

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,6 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('apps.payments.urls')),
-
+    path('api/', include('apps.payments.urls')),
 ]
